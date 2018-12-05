5876a78ae79762ff5959b356ddfc028e8027a537
Lucas POUZAC <lucas.pouzac@lpoconseil.fr>
2018-12-05 17:56:32 +0100

    Refactoring

--- runner.sh
+++ runner.sh
@@ -20,17 +20,26 @@ term_handler() {
 trap 'kill ${!}; term_handler' SIGTERM
 
 
-ln -sf ${SSL_CERTIFICATE} /etc/gitlab-runner/certs/${GITLAB_HOST}.crt
+if [[ ${SSL_CERTIFICATE} ]]; then
+  ln -sf ${SSL_CERTIFICATE} /etc/gitlab-runner/certs/${GITLAB_HOST}.crt
+fi
+
+register_params='--url ${gitlab_service_url}'
+register_params=${register_params}' --registration-token '${GITLAB_RUNNER_TOKEN}
+register_params=${register_params}' --executor docker'
+register_params=${register_params}' --name "runner"'
+register_params=${register_params}' --output-limit "20480"'
+register_params=${register_params}' --docker-image "docker:latest"'
+register_params=${register_params}' --docker-volumes /var/run/docker.sock:/var/run/docker.sock'
+if [[ ${GITLAB_IP} ]]; then
+  register_params=${register_params}' --docker-extra-hosts ${GITLAB_HOST}:${GITLAB_IP}'
+fi
+if [[ ${GITLAB_TAG_LIST} ]]; then
+  register_params=${register_params}' --tag-list "'${GITLAB_TAG_LIST}'"'
+fi
 
 # register runner
-yes '' | gitlab-runner register --url ${gitlab_service_url} \
-                                --registration-token ${GITLAB_RUNNER_TOKEN} \
-                                --executor docker \
-                                --name "runner" \
-                                --output-limit "20480" \
-                                --docker-image "docker:latest" \
-                                --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
-                                --docker-extra-hosts ${GITLAB_HOST}:${GITLAB_IP}
+yes '' | gitlab-runner register ${register_params}
 
 # assign runner token
 token=$(cat /etc/gitlab-runner/config.toml | grep token | awk '{print $3}' | tr -d '"')
